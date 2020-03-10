1c21ee520311d79cb9549dd5747aac6affc05ccb
VladKrivonosov <wonin08@gmail.com>
2020-03-10 21:57:13 +0300

    Task 2 done

---
+++ Task2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList arrayList = new ArrayList();
+
+            //int32
+            arrayList.Add(45); //false
+            arrayList.Add(true); // true unboxing
+            arrayList.Add("Hello"); // true unboxing
+            arrayList.Add(23.45); // true 
+        }
+    }
+}
