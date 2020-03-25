2bee5cabd2a61c32506c7806bdb8bca851d26507
VladKrivonosov <wonin08@gmail.com>
2020-03-26 00:50:07 +0300

    Task_2 done

--- CSharp.Practice/taskLection7/Program.cs
+++ CSharp.Practice/taskLection7/Program.cs
@@ -7,13 +7,36 @@ namespace taskLection7
         static void Main(string[] args)
         {
             Task_1();
-            //Task_2();
+            Task_2();
             //Task_3();
         }
 
         static void Task_1()
+        {
+            string[] array1 = new string[] { };
+        }
+
+        static void Task_2()
+        {
+            object[] myArray = new object[3] { 32, "A", "Hello" };
+            for (int i = 0; i < myArray.Length; i++)
             {
-                string[] array1 = new string[] { };
+                Console.Write(myArray[i] + " ");
             }
+            Console.WriteLine();
+
+            int addNumber = Convert.ToInt32(myArray[0]);
+            addNumber += 10;
+            myArray[0] = addNumber;
+
+            string addText = Convert.ToString(myArray[2]);
+            addText += ", gues !";
+            myArray[2] = addText;
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.Write(myArray[i] + " ");
+            }
+        }
     }
 }
