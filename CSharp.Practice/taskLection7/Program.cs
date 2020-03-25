3cb960e47f0c55dc608b77b846bc5958fee51fb0
VladKrivonosov <wonin08@gmail.com>
2020-03-26 00:26:00 +0300

    task_1 done

---
+++ CSharp.Practice/taskLection7/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace taskLection7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task_1();
+            //Task_2();
+            //Task_3();
+        }
+
+        static void Task_1()
+            {
+                string[] array1 = new string[] { };
+            }
+    }
+}
